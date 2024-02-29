25f65b8f58f9f34c5bcbb8c51d2b6e48cc496540
StanislavIvanov7 <stanislav772000@abv.bg>
2024-02-29 15:36:45 +0200

    Added initials migration for Database

--- Gym.Infrastructure/Data/ApplicationDbContext.cs
+++ Gym.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace Gym.Data
+namespace Gym.Infrastructure.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
--- Gym/Extensions/GymServiceCollectionExtension.cs
+++ Gym/Extensions/GymServiceCollectionExtension.cs
@@ -1,4 +1,4 @@
-﻿using Gym.Data;
+﻿using Gym.Infrastructure.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
