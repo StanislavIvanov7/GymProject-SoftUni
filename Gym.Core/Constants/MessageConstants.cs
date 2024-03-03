bb73464ac5e7792089f5ac5fa2e571fb9bb4cce3
StanislavIvanov7 <stanislav772000@abv.bg>
2024-03-03 23:01:43 +0200

    Added message constants

---
+++ Gym.Core/Constants/MessageConstants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gym.Core.Constants
+{
+    public class MessageConstants
+    {
+        public const string RequiredMessage = "The field {0} is required";
+
+        public const string LengthMessage = "The field {0} must be between {2} and {1} characters long";
+
+    }
+}
