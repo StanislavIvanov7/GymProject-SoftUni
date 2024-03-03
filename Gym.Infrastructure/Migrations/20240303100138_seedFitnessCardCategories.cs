074f2a53972a25dcd60eb65a3d9bf471e4134517
StanislavIvanov7 <stanislav772000@abv.bg>
2024-03-03 12:02:24 +0200

    Add migration for fitness card categories

---
+++ Gym.Infrastructure/Migrations/20240303100138_seedFitnessCardCategories.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Gym.Infrastructure.Migrations
+{
+    public partial class seedFitnessCardCategories : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "FitnessCardCategories",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Group training" },
+                    { 2, "Individual training" },
+                    { 3, "Until 4pm. for men" },
+                    { 4, "Until 4pm. for girls" },
+                    { 5, "Unlimited access for men" },
+                    { 6, "Unlimited access for girls" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "FitnessCardCategories",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "FitnessCardCategories",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "FitnessCardCategories",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "FitnessCardCategories",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "FitnessCardCategories",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "FitnessCardCategories",
+                keyColumn: "Id",
+                keyValue: 6);
+        }
+    }
+}
