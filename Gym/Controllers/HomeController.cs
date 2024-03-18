c25718c4684308005c3500e6d40a50e8e1db8866
StanislavIvanov7 <stanislav772000@abv.bg>
2024-03-18 19:24:44 +0200

    Added about page

--- Gym/Controllers/HomeController.cs
+++ Gym/Controllers/HomeController.cs
@@ -24,6 +24,12 @@ namespace Gym.Controllers
             return View();
         }
 
+        [AllowAnonymous]
+        public IActionResult About()
+        {
+            return View();
+        }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
