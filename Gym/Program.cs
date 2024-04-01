e198885de0fc5d54accc38230028b11e516424a0
StanislavIvanov7 <stanislav772000@abv.bg>
2024-04-01 13:31:31 +0300

    Added Custom Error Pages

--- Gym/Program.cs
+++ Gym/Program.cs
@@ -11,11 +11,13 @@ var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage();
     app.UseMigrationsEndPoint();
 }
 else
 {
-    app.UseExceptionHandler("/Home/Error");
+    app.UseExceptionHandler("/Home/Error/500");
+    app.UseStatusCodePagesWithReExecute("/Home/Error", "?statusCode={0}");
     app.UseHsts();
 }
 
