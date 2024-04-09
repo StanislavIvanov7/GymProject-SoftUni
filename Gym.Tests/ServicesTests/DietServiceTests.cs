3fa506ce2e55b3d84677b3cc631c3dec2316e1ef
StanislavIvanov7 <stanislav772000@abv.bg>
2024-04-09 19:56:12 +0300

    Added  more unit test for diet

--- Gym.Tests/ServicesTests/DietServiceTests.cs
+++ Gym.Tests/ServicesTests/DietServiceTests.cs
@@ -1,10 +1,12 @@
 ﻿using Gym.Core.Contracts;
+using Gym.Core.Models.Diet;
 using Gym.Core.Services;
 using Gym.Infrastructure.Data;
 using Gym.Infrastructure.Data.Common;
 using Gym.Infrastructure.Data.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using NUnit.Framework.Internal;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -56,6 +58,167 @@ namespace Gym.Tests.ServicesTests
             Assert.That(3, Is.EqualTo(dietCollection.Count()));
             Assert.That(dietCollection.Any(h => h.Id == 4), Is.False);
         }
+
+        [Test]
+        public async Task TestDietEdit()
+        {
+         
+            var repo = new Repository(applicationDbContext);
+            dietService = new DietService(repo);
+
+            await repo.AddAsync(new Diet()
+            {
+                Id = 2,
+                DietCategoryId = 1,
+                ImageUrl = "",
+                Title = "",
+                Description = ""
+            });
+
+            await repo.SaveChangesAsync();
+
+            await dietService.EditAsync(2, new DietFormViewModel()
+            {
+                Id = 2,
+                DietCategoryId = 1,
+                ImageUrl = "",
+                Title = "",
+                Description = "This house is edited",
+            });
+
+            var dbDiet = await repo.GetByIdAsync<Diet>(2);
+
+            Assert.That(dbDiet.Description, Is.EqualTo("This house is edited"));
+        }
+
+        [Test]
+        public async Task GetDietForEditTestInMemory()
+        {
+
+            var repo = new Repository(applicationDbContext);
+            dietService = new DietService(repo);
+            
+            await repo.AddRangeAsync(new List<Diet>()
+            {
+                new Diet() { Id = 2, ImageUrl = "", Title = "diet",Description = "",DietCategoryId = 1},
+                new Diet() { Id = 3, ImageUrl = "", Title = "",Description = "",DietCategoryId = 1},
+
+            });
+
+            await repo.SaveChangesAsync();
+
+            var diet = await dietService.GetDietForEditAsync(2);
+
+            Assert.That(2, Is.EqualTo(diet.Id));
+            Assert.AreEqual(diet.Title, "diet");
+        }
+
+        [Test]
+        public async Task GetDietCategoryTestInMemory()
+        {
+
+            var repo = new Repository(applicationDbContext);
+            dietService = new DietService(repo);
+
+            await repo.AddAsync(new DietCategory()
+            {
+                Id = 4,
+                Name = ""
+
+            });
+
+            await repo.SaveChangesAsync();
+
+            var dietCollection = await dietService.GetDietCategoriesAsync();
+
+            Assert.That(4, Is.EqualTo(dietCollection.Count()));
+            Assert.That(dietCollection.Any(h => h.Id == 5), Is.False);
+        }
+
+        [Test]
+        public async Task GetDietForDeleteTestInMemory()
+        {
+
+            var repo = new Repository(applicationDbContext);
+            dietService = new DietService(repo);
+
+            await repo.AddRangeAsync(new List<Diet>()
+            {
+                new Diet() { Id = 2, ImageUrl = "", Title = "diet",Description = "",DietCategoryId = 1},
+                new Diet() { Id = 3, ImageUrl = "", Title = "",Description = "",DietCategoryId = 1},
+
+            });
+
+            await repo.SaveChangesAsync();
+
+            var diet = await dietService.GetDietForDeleteAsync(2);
+
+            Assert.That(2, Is.EqualTo(diet.Id));
+            Assert.AreEqual(diet.Title, "diet");
+        }
+
+        [Test]
+        public async Task ExistDietTestInMemory()
+        {
+
+            var repo = new Repository(applicationDbContext);
+            dietService = new DietService(repo);
+
+            await repo.AddRangeAsync(new List<Diet>()
+            {
+                new Diet() { Id = 2, ImageUrl = "", Title = "diet",Description = "",DietCategoryId = 1},
+                new Diet() { Id = 3, ImageUrl = "", Title = "",Description = "",DietCategoryId = 1},
+
+            });
+
+            await repo.SaveChangesAsync();
+
+            var diet = await dietService.ExistAsync(2);
+
+            Assert.IsTrue(diet);
+        }
+
+        [Test]
+        public async Task CategoryExistDietTestInMemory()
+        {
+
+            var repo = new Repository(applicationDbContext);
+            dietService = new DietService(repo);
+
+            await repo.AddAsync(new DietCategory()
+            {
+                Id = 4,
+                Name = ""
+
+            });
+
+            await repo.SaveChangesAsync();
+
+            var diet = await dietService.CategoryExistAsync(2);
+
+            Assert.IsTrue(diet);
+        }
+        //[Test]
+        //public async Task DetailsDietTestInMemory()
+        //{
+
+        //    var repo = new Repository(applicationDbContext);
+        //    dietService = new DietService(repo);
+
+        //    await repo.AddRangeAsync(new List<Diet>()
+        //    {
+        //        new Diet() { Id = 2, ImageUrl = "", Title = "", Description = "", CreatorId = "2a2dba3e-f9bf-4c83-83eb-fbd8af5f891c",DietCategoryId = 1},
+        //        new Diet() { Id = 3, ImageUrl = "", Title = "",Description = "",CreatorId = "2a2dba3e-f9bf-4c83-83eb-fbd8af5f891c",DietCategoryId = 1},
+
+        //    });
+
+        //    await repo.SaveChangesAsync();
+
+        //    var diet = await dietService.DetailsDietAsync(1);
+
+        //    Assert.AreEqual(diet.Title, "The best diet for weight loss");
+        //    Assert.AreEqual(diet.Id,1);
+        //}
 
         [TearDown]
         public void TearDown()
