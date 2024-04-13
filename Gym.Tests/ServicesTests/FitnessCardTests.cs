a310cb79cf405b1f72d72a9cfdd962786484069e
StanislavIvanov7 <stanislav772000@abv.bg>
2024-04-13 14:24:41 +0300

    Implemented unit tests for fitness card service

---
+++ Gym.Tests/ServicesTests/FitnessCardTests.cs
@@ -0,0 +1,200 @@
+﻿using Gym.Core.Contracts;
+using Gym.Infrastructure.Data.Common;
+using Gym.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Gym.Core.Models.FitnessCard;
+using Gym.Core.Models.Diet;
+using Gym.Core.Services;
+using Gym.Infrastructure.Data.Models;
+
+namespace Gym.Tests.ServicesTests
+{
+    [TestFixture]
+    public class FitnessCardTests
+    {
+        private IRepository repo;
+        private IFitnessCardService fitnessCardService;
+        private ApplicationDbContext applicationDbContext;
+
+        [SetUp]
+        public void Setup()
+        {
+
+            var contextOptions = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase("HouseDB")
+                .Options;
+
+            applicationDbContext = new ApplicationDbContext(contextOptions);
+
+            applicationDbContext.Database.EnsureDeleted();
+            applicationDbContext.Database.EnsureCreated();
+        }
+
+        [Test]
+        public async Task AllFitnessCardTestInMemory()
+        {
+
+            var repo = new Repository(applicationDbContext);
+            fitnessCardService = new FitnessCardService(repo);
+
+            await repo.AddRangeAsync(new List<FitnessCard>()
+            {
+                new FitnessCard() { Id = 2, ImageUrl = "", Name = ""},
+                new FitnessCard() { Id = 3, ImageUrl = "", Name = ""},
+            });
+            await repo.SaveChangesAsync();
+
+            var dietCollection = await fitnessCardService.AllFitnessCardAsync();
+
+            Assert.That(3, Is.EqualTo(dietCollection.Count()));
+            Assert.That(dietCollection.Any(h => h.Id == 4), Is.False);
+        }
+
+        [Test]
+        public async Task TestFitnessCardEdit()
+        {
+
+            var repo = new Repository(applicationDbContext);
+            fitnessCardService = new FitnessCardService(repo);
+
+            await repo.AddAsync(new FitnessCard()
+            {
+                Id = 2,
+                FitnessCardCategoryId = 1,
+                ImageUrl = "",
+                Name = "",
+                Description = ""
+            });
+            await repo.SaveChangesAsync();
+
+            await fitnessCardService.EditAsync(2, new FitnessCardFormViewModel()
+            {
+                Id = 2,
+                FitnessCardCategoryId = 1,
+                ImageUrl = "",
+                Name = "",
+                Description = "This fitness card is edited",
+            });
+
+            var dbDiet = await repo.GetByIdAsync<FitnessCard>(2);
+
+            Assert.That(dbDiet.Description, Is.EqualTo("This fitness card is edited"));
+        }
+
+        [Test]
+        public async Task GetFitnessCardForEditTestInMemory()
+        {
+
+            var repo = new Repository(applicationDbContext);
+            fitnessCardService = new FitnessCardService(repo);
+
+            await repo.AddRangeAsync(new List<FitnessCard>()
+            {
+                new FitnessCard() { Id = 2, ImageUrl = "", Name = "fitness card",Description = "",FitnessCardCategoryId = 1},
+                new FitnessCard() { Id = 3, ImageUrl = "", Name = "",Description = "",FitnessCardCategoryId = 1},
+
+            });
+            await repo.SaveChangesAsync();
+
+            var diet = await fitnessCardService.GetFitnessCardForEditAsync(2);
+
+            Assert.That(2, Is.EqualTo(diet.Id));
+            Assert.AreEqual(diet.Name, "fitness card");
+        }
+
+        [Test]
+        public async Task GetFitnessCardCategoryTestInMemory()
+        {
+
+            var repo = new Repository(applicationDbContext);
+            fitnessCardService = new FitnessCardService(repo);
+
+            await repo.AddAsync(new FitnessCardCategory()
+            {
+                Id = 7,
+                Name = ""
+
+            });
+            await repo.SaveChangesAsync();
+
+            var fitnessCardCollection = await fitnessCardService.GetFitnessCardCategoryAsync();
+
+            Assert.That(7, Is.EqualTo(fitnessCardCollection.Count()));
+            Assert.That(fitnessCardCollection.Any(h => h.Id == 8), Is.False);
+        }
+
+        [Test]
+        public async Task GetFitnessCardForDeleteTestInMemory()
+        {
+
+            var repo = new Repository(applicationDbContext);
+            fitnessCardService = new FitnessCardService(repo);
+
+            await repo.AddRangeAsync(new List<FitnessCard>()
+            {
+                new FitnessCard() { Id = 2, ImageUrl = "", Name = "fitnessCard",Description = "",FitnessCardCategoryId = 1},
+                new FitnessCard() { Id = 3, ImageUrl = "", Name = "",Description = "",FitnessCardCategoryId = 1},
+
+            });
+            await repo.SaveChangesAsync();
+
+            var diet = await fitnessCardService.GetFitnessCardForDeleteAsync(2);
+
+            Assert.That(2, Is.EqualTo(diet.Id));
+            Assert.AreEqual(diet.Name, "fitnessCard");
+        }
+
+        [Test]
+        public async Task ExistFitnessCardTestInMemory()
+        {
+
+            var repo = new Repository(applicationDbContext);
+            fitnessCardService = new FitnessCardService(repo);
+
+            await repo.AddRangeAsync(new List<FitnessCard>()
+            {
+                new FitnessCard() { Id = 2, ImageUrl = "", Name = "fitnessCard",Description = "",FitnessCardCategoryId = 1},
+                new FitnessCard() { Id = 3, ImageUrl = "", Name = "",Description = "",FitnessCardCategoryId = 1},
+
+            });
+            await repo.SaveChangesAsync();
+
+            var diet = await fitnessCardService.ExistAsync(2);
+
+            Assert.IsTrue(diet);
+        }
+
+        [Test]
+        public async Task CategoryExistFitnessCardTestInMemory()
+        {
+
+            var repo = new Repository(applicationDbContext);
+            fitnessCardService = new FitnessCardService(repo);
+
+            await repo.AddAsync(new FitnessCardCategory()
+            {
+                Id = 7,
+                Name = ""
+
+            });
+            await repo.SaveChangesAsync();
+
+            var diet = await fitnessCardService.CategoryExistAsync(2);
+
+            Assert.IsTrue(diet);
+        }
+
+
+        [TearDown]
+        public void TearDown()
+        {
+            applicationDbContext.Dispose();
+        }
+
+    }
+}
